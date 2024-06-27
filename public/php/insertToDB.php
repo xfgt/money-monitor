5573aa6f31035f69d07231e7a4463d5cb6c97c27
xfgt <waffle_man562@yahoo.com>
2024-06-27 13:42:37 +0300

    changed fucntionality

--- public/php/insertToDB.php
+++ public/php/insertToDB.php
@@ -7,13 +7,29 @@ $con = mysqli_connect('localhost', 'root', '','moneysavingsdb');
 // get the post records
 //$name = $_POST['container'];
 
+$action;
 
-$action = $_POST[''];
-$amount = $_POST[''];
-$description = $_POST[''];
+
+$actionIn = $_GET['ActionIn'];
+$actionOut = $_GET['ActionOut'];
+
+
+if($actionIn == 1){
+	$action = $actionIn;
+	$amount = $_GET['AmountAdded'];
+	$description = $_GET['ArgumentsIncome'];
+}
+
+
+if($actionOut == 0){
+	$action = $actionOut;
+	$amount = $_GET['AmountDrawn'];
+	$description = $_GET['ArgumentsOutcome'];
+}
+
 
 // database insert SQL code
-$sql = "INSERT INTO `history` (`ACTION`, `AMOUNT`, `DESCRIPTION`) VALUES ('$action', '$amount', '$description')";
+$sql = "INSERT INTO 'history' ('ACTION', 'AMOUNT', 'DESCRIPTION') VALUES ('$action', '$amount', '$description')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);
@@ -21,6 +37,7 @@ $rs = mysqli_query($con, $sql);
 if($rs)
 {
 	echo "Информацията е вкарана!";
-}
+
+} else echo "проблем :/";
 
 ?>
