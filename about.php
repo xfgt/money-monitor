ed7fe223fc53518c1d82111027818a0370b52495
xfgt <xfgt_71@proton.me>
2026-08-15 20:05:50 +0300

    header tag convention and placeholder text

--- about.php
+++ about.php
@@ -1,25 +1,33 @@
+<?php
+$pageTitle =  "Money monitor - About";
+require_once 'header.php'
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, text/html" />
     <link rel="stylesheet" href="public/src/css/page.css">
-    <link rel="stylesheet" href="public/src/css/top_bottom-bars.css">
     <link rel="icon" href="public/img/fc.gif" type="image/gif">
+    <title><?php echo htmlspecialchars($pageTitle); ?></title>
 
-    <title>About</title>
+
+    
 </head>
 <body>
     
-
-
-
-
-    <div class="bottomBarClass">
-        <div id="me">
-            <a href="https://github.com/xfgt">Author</a>
-            <a href="mailto:xfgt_71@proton.me">Contact</a>
-        </div>
-    </div>
 </body>
+
+    <h1> About this project </h1>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+    </p>
+
+
+
+<?php
+require_once 'footer.php'
+?>
 </html>
