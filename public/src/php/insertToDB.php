1762b0c859e47284f9c19cf2b91610079d5b4f72
xfgt <xfgt_71@proton.me>
2026-08-16 09:50:53 +0300

    comment out

--- public/src/php/insertToDB.php
+++ public/src/php/insertToDB.php
@@ -1,46 +1,46 @@
 <?php
 
-$user = 'root';
-$password = '';
-$database = 'moneysavingsdb';
-$servername='localhost:3306';
+// $user = 'root';
+// $password = '';
+// $database = 'moneysavingsdb';
+// $servername='localhost:3306';
 
-$con = new mysqli($servername, $user, $password, $database);
+// $con = new mysqli($servername, $user, $password, $database);
 
-$action = $_GET['Action'];
-$timestamp = $_GET['time-income'];
-
-
-if($action == 1){
-	$amount = $_GET['AmountAdded'];
-	$description = $_GET['ArgumentsIncome'];
-}
-
-if($action == 0){
-	$amount = $_GET['AmountDrawn'];
-	$description = $_GET['ArgumentsOutcome'];
-}
-
-if(!$timestamp){
-
-	$sql = "INSERT INTO history (ACTION, AMOUNT, DESCRIPTION) VALUES ($action, $amount, '$description')";
-} else {
-	$sql = "INSERT INTO history (TIMEDATE, ACTION, AMOUNT, DESCRIPTION) VALUES ($timestamp, $action, $amount, '$description')";
-}
+// $action = $_GET['Action'];
+// $timestamp = $_GET['time-income'];
 
 
-// $sql = file_get_contents('../db/insertTodb.sql');
-$result = $con->query($sql);
+// if($action == 1){
+// 	$amount = $_GET['AmountAdded'];
+// 	$description = $_GET['ArgumentsIncome'];
+// }
 
-if($result)
-{
-	echo "<h2>Информацията бе вкарана успешно!</h2>";
+// if($action == 0){
+// 	$amount = $_GET['AmountDrawn'];
+// 	$description = $_GET['ArgumentsOutcome'];
+// }
 
-} else echo "<h2>проблем :/</h2>";
+// if(!$timestamp){
 
-header("Location: ../../");
-die();
+// 	$sql = "INSERT INTO history (ACTION, AMOUNT, DESCRIPTION) VALUES ($action, $amount, '$description')";
+// } else {
+// 	$sql = "INSERT INTO history (TIMEDATE, ACTION, AMOUNT, DESCRIPTION) VALUES ($timestamp, $action, $amount, '$description')";
+// }
 
 
-//$con->close();
+// // $sql = file_get_contents('../db/insertTodb.sql');
+// $result = $con->query($sql);
+
+// if($result)
+// {
+// 	echo "<h2>Информацията бе вкарана успешно!</h2>";
+
+// } else echo "<h2>проблем :/</h2>";
+
+// header("Location: ../../");
+// die();
+
+
+// //$con->close();
 ?>
