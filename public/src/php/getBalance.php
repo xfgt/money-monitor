37ca01aac3cc662e22c144ab621e5c4a8dfd3d0a
xfgt <xfgt_71@proton.me>
2026-08-16 09:37:09 +0300

    comment out unnecessary code for later fix

--- public/src/php/getBalance.php
+++ public/src/php/getBalance.php
@@ -1,34 +1,55 @@
 <?php
-$user = 'root';
-$password = '';
-$database = 'moneysavingsdb';
-$servername='localhost:3306';
 
-$mysqli = new mysqli($servername, $user, $password, $database);
 
-// Checking for connections
-if ($mysqli->connect_error) {
-    die('Connect Error (' .
-    $mysqli->connect_errno . ') '.
-    $mysqli->connect_error);
-}
 
-$selectSumIn = 'SELECT SUM(AMOUNT) FROM history WHERE ACTION=1';
-$selectSumOut = 'SELECT SUM(AMOUNT) FROM history WHERE ACTION=0';
 
-$positive = get_value($mysqli, $selectSumIn);
-$negative = get_value($mysqli, $selectSumOut);
 
-$res = $positive - $negative;
-echo $res;
+echo $statusClass . $user; // test
 
-//$res = $q1 - $q2;
 
-$mysqli->close();
 
-function get_value($mysqli, $sql) {
-    $result = $mysqli->query($sql);
-    $value = $result->fetch_array(MYSQLI_NUM);
-    return is_array($value) ? $value[0] : "";
-}
+
+
+
+
+
+
+
+
+
+
+
+// $user = 'root';
+// $password = '';
+// $database = 'moneysavingsdb';
+// $servername='localhost:3306';
+
+// $mysqli = new mysqli($servername, $user, $password, $database);
+
+// // Checking for connections
+// if ($mysqli->connect_error) {
+//     die('Connect Error (' .
+//     $mysqli->connect_errno . ') '.
+//     $mysqli->connect_error);
+// }
+
+// $selectSumIn = 'SELECT SUM(AMOUNT) FROM history WHERE ACTION=1';
+// $selectSumOut = 'SELECT SUM(AMOUNT) FROM history WHERE ACTION=0';
+
+// $positive = get_value($mysqli, $selectSumIn);
+// $negative = get_value($mysqli, $selectSumOut);
+
+// $res = $positive - $negative;
+// echo $res;
+
+// //$res = $q1 - $q2;
+
+// $mysqli->close();
+
+// function get_value($mysqli, $sql) {
+//     $result = $mysqli->query($sql);
+//     $value = $result->fetch_array(MYSQLI_NUM);
+//     return is_array($value) ? $value[0] : "";
+// }
+
 ?>
--- public/src/php/selectAllFromDB.php
+++ public/src/php/selectAllFromDB.php
@@ -1,24 +1,24 @@
 <?php
  
-$user = 'root';
-$password = '';
-$database = 'moneysavingsdb';
+// $user = 'root';
+// $password = '';
+// $database = 'moneysavingsdb';
 
 
-$servername='localhost:3306';
+// $servername='localhost:3306';
 
-$mysqli = new mysqli($servername, $user, $password, $database);
+// $mysqli = new mysqli($servername, $user, $password, $database);
 
-// Checking for connections
-if ($mysqli->connect_error) {
-    die('Connect Error (' .
-    $mysqli->connect_errno . ') '.
-    $mysqli->connect_error);
-}
+// // Checking for connections
+// if ($mysqli->connect_error) {
+//     die('Connect Error (' .
+//     $mysqli->connect_errno . ') '.
+//     $mysqli->connect_error);
+// }
 
 
-$sql = " SELECT * FROM history ORDER BY TIMEDATE DESC ";
-$result = $mysqli->query($sql);
-$mysqli->close();
+// $sql = " SELECT * FROM history ORDER BY TIMEDATE DESC ";
+// $result = $mysqli->query($sql);
+// $mysqli->close();
     
 ?>
